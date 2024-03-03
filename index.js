a71c75e9183899abbf68dcbb7c10cf565a196d12
sai <ishwarikape117@gmail.com>
2024-03-03 08:08:47 -0800

    added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const url = "https://api.github.com/users";
-const searchInput=document.getElementById(searchInput);
+const searchInputE1 = document.getElementById("searchInput");
 const searchButtonEl = document.getElementById("searchBtn");
+const profileContainer = document.getElementById("profileContainer");
+const loading = document.getElementById("loading");
+
+const generateProfile=(profile) => {
+    return  `
+    <div class="profileBox">
+    <div class="top-section">
+    <div class="left">
+    <div class="avtar">
+      <img alt="avtar" src="${"profile.avtar_url"} />
+    </div>
+    <div class="self">
+     <h1>${profile.name}</h1>
+     <h1>@${profile.login}</h1>
+    </div>
+    </div>
+    <a href="${profile.html_url}" target="_black">
+     <button class="primary-btn">Check Profile</button>
+     </a>
+    </div>
+`;
+};
